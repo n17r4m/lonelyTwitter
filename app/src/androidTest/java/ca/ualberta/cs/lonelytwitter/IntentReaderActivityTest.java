ffe231315b1e381b5865829f9329d6d97e78eccb
Ali Sajedi Badashian <sajediba@cs.ualberta.ca>
2016-02-11 15:19:05 -0700

    End of lab.

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/IntentReaderActivityTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/IntentReaderActivityTest.java
@@ -16,7 +16,38 @@ public class IntentReaderActivityTest extends ActivityInstrumentationTestCase2{
 
     //
     //
+    public void testSendText(){
+        Intent intent = new Intent();
+        intent.putExtra(IntentReaderActivity.TEXT_TO_TRANSFORM_KEY, "Test message 2");
+        setActivityIntent(intent);
 
+        IntentReaderActivity intentReaderActivity = (IntentReaderActivity) getActivity();
+        assertEquals("IntentReaderActivity should get the text from intent!",
+                "Test message 2", intentReaderActivity.getText());
+    }
+
+    public void testDisplayText(){
+        Intent intent = new Intent();
+        intent.putExtra(IntentReaderActivity.TEXT_TO_TRANSFORM_KEY, "Test message 3");
+        setActivityIntent(intent);
+
+        IntentReaderActivity intentReaderActivity = (IntentReaderActivity) getActivity();
+        TextView textView = (TextView) intentReaderActivity.findViewById(R.id.intentText);
+        assertEquals("Text should be displayed in the TextView!",
+                textView.getText().toString(), "Test message 3");
+    }
+
+    public void testDoubleText(){
+        Intent intent = new Intent();
+        intent.putExtra(IntentReaderActivity.TEXT_TO_TRANSFORM_KEY, "Test message 4");
+        intent.putExtra(IntentReaderActivity.MODE_OF_TRANSFORM_KEY, IntentReaderActivity.DOUBLE);
+        setActivityIntent(intent);
+
+        IntentReaderActivity intentReaderActivity = (IntentReaderActivity) getActivity();
+        assertEquals("The text should be repeated!", "Test message 4Test message 4",
+                intentReaderActivity.getText());
+
+    }
     //
     //
 
