0a8abf6e362e89d1690526c2c3c7a158e333af45
Martin Humphreys <n17r4m@gmail.com>
2016-02-11 15:38:13 -0700

    added tests

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/IntentReaderActivityTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/IntentReaderActivityTest.java
@@ -2,7 +2,9 @@ package ca.ualberta.cs.lonelytwitter;
 
 import android.content.Intent;
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.UiThreadTest;
 import android.test.ViewAsserts;
+import android.view.View;
 import android.widget.TextView;
 
 /**
@@ -48,6 +50,48 @@ public class IntentReaderActivityTest extends ActivityInstrumentationTestCase2{
                 intentReaderActivity.getText());
 
     }
+
+
+    @UiThreadTest
+    public void testReverseText(){
+
+        Intent intent = new Intent();
+        intent.putExtra(IntentReaderActivity.TEXT_TO_TRANSFORM_KEY, "12345");
+        intent.putExtra(IntentReaderActivity.MODE_OF_TRANSFORM_KEY, IntentReaderActivity.REVERSE);
+        setActivityIntent(intent);
+
+        IntentReaderActivity intentReaderActivity = (IntentReaderActivity) getActivity();
+        assertEquals("The text should be repeated!", "54321",
+                intentReaderActivity.getText());
+
+    }
+
+    @UiThreadTest
+    public void testDefaultText(){
+
+        Intent intent = new Intent();
+        setActivityIntent(intent);
+
+        IntentReaderActivity intentReaderActivity = (IntentReaderActivity) getActivity();
+
+        assertEquals("IntentReaderActivity should have a default text value!",
+                "default value", intentReaderActivity.getText());
+
+    }
+
+    @UiThreadTest
+    public void testShownText(){
+
+        Intent intent = new Intent();
+        setActivityIntent(intent);
+
+        IntentReaderActivity intentReaderActivity = (IntentReaderActivity) getActivity();
+        View decorView = intentReaderActivity.getWindow().getDecorView();
+        TextView textView = (TextView) intentReaderActivity.findViewById(R.id.intentText);
+        ViewAsserts.assertOnScreen(decorView, textView);
+
+    }
+
     //
     //
 
